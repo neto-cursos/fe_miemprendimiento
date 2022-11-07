493e4f979b55767087fcfe8572115cf112eb05a4
neto-cursos <neto.cursos.37@gmail.com>
2022-11-06 21:09:34 -0400

    3er cambio

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -2,10 +2,10 @@ import { FormAction, FormExtra, Input, Header } from "./Elements";
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { loginFields } from '../../constants/formFields'
+import { loginFields } from './../../constants/formFields'
 import Error from '../Error'
 import * as yup from "yup";
-import { userLogin } from '../../redux/actions/userActions';
+import { userLogin } from './../../redux/actions/userActions';
 
 
 
