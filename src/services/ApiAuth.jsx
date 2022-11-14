715af257745faca14d1dc4de7fd523b43933b0e5
neto-cursos <neto.cursos.37@gmail.com>
2022-11-13 23:49:13 -0400

    13vo cambio

--- src/services/ApiAuth.jsx
+++ src/services/ApiAuth.jsx
@@ -7,9 +7,9 @@ const ApiAuth = () => {
     console.log('token: ' + token);
 
     const api = axios.create({
-        baseURL: 'http://localhost:8000/api',
+        // baseURL: 'http://localhost:8000/api',
         // baseURL: 'http://44.202.39.9/public/api',
-        // baseURL: 'api',
+        baseURL: 'api',
         withCredentials: true,
         headers: {
             Authorization: `${token}`,
